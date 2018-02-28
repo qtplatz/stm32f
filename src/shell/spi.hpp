fa91b57b57b24941468b9e0e115ed0014bf6ddf1
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2018-02-28 23:47:49 +0900

    soft ss control trail on spi2

--- src/shell/spi.hpp
+++ src/shell/spi.hpp
@@ -5,6 +5,8 @@
 
 namespace stm32f103 {
 
+    template< typename T > class gpio;
+    
     enum SPI_BASE : uint32_t;
     struct SPI;
 
@@ -12,15 +14,29 @@ namespace stm32f103 {
         volatile SPI * spi_;
         std::atomic_flag lock_;
         std::atomic< uint32_t > rxd_;
+        
+        // workaround -- Initially, I thought GPIO and SPI controls are fully independent,
+        // bit this peripheral seems exepecting a ~ss line control by software using GPIO.
+        // I've designed GPIO control based on c++ type dispatch, but not for SPI.
+        uint8_t gpio_;  // A|B|none
+        uint32_t ss_n_;  // PA4|PB
     public:
-        void init( SPI_BASE );
+        void init( SPI_BASE, uint8_t gpio = 0, uint32_t ss_n = 0 );
         inline operator bool () const { return spi_; };
         
         spi& operator << ( uint16_t );
+        
+        void operator = ( bool flag ); // SS control
 
         void handle_interrupt();
         static void interrupt_handler( spi * );
     };
 
+    template< typename GPIO_PIN_type >
+    class spi_t : public spi {
+        GPIO_PIN_type ss_n_;
+    public:
+        template< GPIO_PIN_type > void init( SPI_BASE, GPIO_PIN_type ss_n );
+    };
 }
 
